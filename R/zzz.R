2412a00cc9056ad6ab2cd54d004ec29e9a791562
Xiaopei <105765429+ScottCalcalas@users.noreply.github.com>
2026-01-15 15:19:23 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "XZDBfunction loaded! 🚀\n",
-    "Use XZDB.Run() to start the Shiny Genomic DB Browser.\n",
-    "Need help? Run: ?XZDB.Run \n",
-    "Update me: XZ.update()"
+    "--Use XZDB.Run() to start the Shiny Genomic DB Browser.\n",
+    "---Need help? Run: ?XZDB.Run \n",
+    "----Update me: XZ.update()"
   )
 }
